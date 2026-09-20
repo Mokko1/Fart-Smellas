2b13c14aea39d283545b97d024607ee4fa01cd6e
NebSeniah <bwh8037@g.rit.edu>
2026-09-20 11:11:18 -0400

    Create a template quest
    Created a template quest when tab is pressed, got the mod to run successfully
    (Note for the future, you probably need to copy the current content.json into the mod folder before running it)

---
+++ SmartFellasMod/ModEntry.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+using StardewValley.Quests;
+
+namespace SmartFellasMod
+{
+    /// <summary>The mod entry point.</summary>
+    internal sealed class ModEntry : Mod
+    {
+        /*********
+        ** Public methods
+        *********/
+        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+        /// <param name="helper">Provides simplified APIs for writing mods.</param>
+        public override void Entry(IModHelper helper)
+        {
+            helper.Events.Input.ButtonPressed += this.OnButtonPressed;
+        }
+
+
+        /*********
+        ** Private methods
+        *********/
+        /// <summary>Raised after the player presses a button on the keyboard, controller, or mouse.</summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event data.</param>
+        private void OnButtonPressed(object? sender, ButtonPressedEventArgs e)
+        {
+
+            if (!Context.IsWorldReady)
+                return;
+
+            this.Monitor.Log($"{Game1.player.Name} pressed {e.Button}.", LogLevel.Info);
+
+            if (e.Button == SButton.Tab)
+                StartExampleQuest();
+        }
+
+        private void StartExampleQuest()
+        {
+            var exampleQuest = new Quest();
+            exampleQuest.id.Value = "999";
+            exampleQuest.questType.Value = 3;
+
+            exampleQuest.questTitle = "Cool Test Quest B)";
+            exampleQuest.questDescription = "This is a very cool template quest";
+            exampleQuest.moneyReward.Value = 500;
+
+
+            Game1.player.questLog.Add(exampleQuest);
+        }
+
+    }
+}
